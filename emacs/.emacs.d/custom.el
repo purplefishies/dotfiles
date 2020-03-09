8906c3acab36bdb629d4232c959c6497773602d7
Jimi Damon <jdamon@gmail.com>
2020-03-09 11:17:58 -0700

    Adding emacs startup code

---
+++ emacs/.emacs.d/custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ '(epa-file-cache-passphrase-for-symmetric-encryption t t)
+ '(markdown-command "pandoc ")
+ '(org-agenda-files
+   (list "~/Projects/org/archive.org" "~/Projects/org/getting_organized.org" "~/Projects/org/personal.org" "~/Projects/org/archive.org" "~/Projects/org/todo.org" "~/Projects/org/work_projects.org" "~/Dropbox/inbox.org"))
+ '(org-checkbox-hierarchical-statistics nil)
+ '(org-lowest-priority 68)
+ '(org-modules
+   (quote
+    (org-bbdb org-bibtex org-ctags org-docview org-gnus org-info org-irc org-mhe org-rmail org-w3m org-drill org-learn)))
+ '(org-priority-start-cycle-with-default t)
+ '(package-selected-packages
+   (quote
+    (org-plus-contrib gnu-elpa-keyring-update org-journal org-drill kotlin-mode ensime cedit cdlatex ledger-import flycheck-ledger org-babel-eval-in-repl graphviz-dot-mode dot-mode org-drill-table dash yaml-mode scala-mode polymode passthword org-bullets org neotree markdown-mode json-mode groovy-mode gradle-mode gitignore-mode color-theme-modern cmake-mode chess bind-key auto-complete auctex))))
+(custom-set-faces
+ '(org-agenda-done ((t (:foreground "sea green")))))
---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,22 @@
+(setq inhibit-splash-screen t)
+;; Configure package.el to include MELPA.
+
+(require 'package)
+(when (>= emacs-major-version 24)
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  )
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; Ensure that use-package is installed.
+;; If use-package isn't already installed, it's extremely likely that this is a
+;; fresh installation! So we'll want to update the package repository and
+;; install use-package before loading the literate configuration.
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+(org-babel-load-file "~/.emacs.d/configuration.org")
+
